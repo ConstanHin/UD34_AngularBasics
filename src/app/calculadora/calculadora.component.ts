15d96ed5b302ab62a7a35f3924f26ca63dcefa0d
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-04 00:58:31 +0200

    typescripe componenete calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.scss']
 })
 export class CalculadoraComponent implements OnInit {
+  operando1: number;
+  operando2: number;
+  operador: string;
+  btnsNumber : NodeListOf<HTMLElement>;
 
-  constructor() { }
+  constructor() {
+    this.operando1 = 0;
+    this.operando2 = 0;
+    this.operador = "";
+    this.btnsNumber = document.querySelectorAll(".calc-left-btn");
+
+  }
 
   ngOnInit(): void {
+    this.btnsNumber.forEach(btn => {
+      btn.click = this.numberEvent;
+    });
   }
+
+  numberEvent(){
+    console.log("clicado");
+
+  }
+
 
 }
